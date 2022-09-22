e9fe0d386f3ebb828a6638883e280251c57cef0b
hugydugy <dlalswns456@kakao.com>
2022-09-22 19:55:01 +0900

    명령어 입력

--- src/Main.java
+++ src/Main.java
@@ -8,12 +8,16 @@ public class Main {
 
 		Scanner sc = new Scanner(System.in);
 
-		String cmd = sc.nextLine();
+		while (true) {
+			System.out.printf("명령어) ");
+			String cmd = sc.nextLine();
 
-		System.out.println("입력된 명령어 : " + cmd);
+			if (cmd.equals("exit")) {
+				break;
+			}
+		}
 
 		System.out.println("== 프로그램 끝 ==");
-
 		sc.close();
 	}
 }
